67bff8753632f8c3dc65ebb7bc80bde674c019f8
Ruben Madila <contact@rubenmadila.com>
2019-08-28 08:29:00 +0100

    Removed typography

--- functions.php
+++ functions.php
@@ -153,8 +153,6 @@ function indigo_scripts() {
 
 	wp_enqueue_script( 'indigo-script', get_template_directory_uri() . '/assets/scripts.js', array(), '20151215', true );
 
-	wp_enqueue_style( 'indigo-fonts', 'https://fonts.googleapis.com/css?family=Hepta+Slab:200,300,400,500,600&display=swap', array(), null, true);
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
