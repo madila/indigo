cb3f043f62ffdfb8f824f7e2f9f559f91f05fdfb
Ruben Madila <contact@rubenmadila.com>
2019-09-16 08:10:43 +0100

    refresh assets

--- functions.php
+++ functions.php
@@ -162,11 +162,11 @@ function indigo_get_theme_style() {
  */
 function indigo_scripts() {
 
-	wp_enqueue_style( 'indigo-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'indigo-style', get_stylesheet_uri(), array(), '20190916' );
 
-	wp_enqueue_style( 'indigo-theme', indigo_get_theme_style(), array(), '20151215' );
+	wp_enqueue_style( 'indigo-theme', indigo_get_theme_style(), array(), '20190916' );
 
-	wp_enqueue_script( 'indigo-script', get_template_directory_uri() . '/js/scripts.js', array(), '20151215', true );
+	wp_enqueue_script( 'indigo-script', get_template_directory_uri() . '/js/scripts.js', array(), '20190916', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
