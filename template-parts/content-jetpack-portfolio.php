e769cc648438f6368e1795b38e7f84b3a2bc832e
Ruben Madila <contact@rubenmadila.com>
2019-09-17 10:07:39 +0100

    remove title from portfolio

--- template-parts/content-jetpack-portfolio.php
+++ template-parts/content-jetpack-portfolio.php
@@ -10,13 +10,23 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header class="entry-header screen-reader-text">
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif; ?>
+		endif;
+
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta">
+				<?php
+				indigo_posted_on();
+				indigo_posted_by();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<?php indigo_post_thumbnail(); ?>
@@ -25,7 +35,7 @@
 		<?php
 		the_content( sprintf(
 			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
+			/* translators: %s: Name of current post. Only visible to screen readers */
 				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'indigo' ),
 				array(
 					'span' => array(
