4e1ad51164f4427264e80ab0278aeedba4775ac1
Ruben Madila <contact@rubenmadila.com>
2019-10-21 13:32:20 +0100

    section cover filters

--- template-parts/content-covers.php
+++ template-parts/content-covers.php
@@ -12,8 +12,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('section section-cover two-thirds-height'); ?>>
 	<?php indigo_post_thumbnail(true); ?>
 	<div class="section-overlay">
-		<a class="section-content v-align text-center" href="<?php the_permalink(); ?>">
-			<div class="v-align-middle">
+		<a class="section-content v-align text-<?php echo apply_filters('section_cover_horizontal_align', 'center'); ?>" href="<?php the_permalink(); ?>">
+			<div class="v-align-<?php echo apply_filters('section_cover_vertical_align', 'middle'); ?>">
 				<?php the_title('<h2 class="display-4 my-0">', '</h2>'); ?>
 				<?php if(has_excerpt(get_the_ID())) { ?>
 					<hr/>
