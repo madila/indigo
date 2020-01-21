e2f55d993228a3b25e58a8eb6799c1c5d31a9b9e
Ruben Madila <contact@rubenmadila.com>
2020-01-21 13:14:56 +0000

    editor style

--- functions.php
+++ functions.php
@@ -109,6 +109,8 @@ if ( ! function_exists( 'indigo_setup' ) ) :
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
+		add_theme_support( 'responsive-embeds' );
+
 		/**
 		 * Add support for core custom logo.
 		 *
@@ -206,7 +208,7 @@ add_action( 'wp_enqueue_scripts', 'indigo_scripts' );
  * Enqueue block editor style
  */
 function indigo_block_editor_styles() {
-	wp_enqueue_style( 'block-editor-styles', get_theme_file_uri( '/block-editor-style.css' ), false, '1.0', 'screen' );
+	wp_enqueue_style( 'block-editor-styles', get_template_directory_uri().'/block-editor-style.css', false, '1.0', 'screen' );
 }
 add_action( 'enqueue_block_editor_assets', 'indigo_block_editor_styles' );
 
