3b2c0cadb9b48448020d69daef6d43b8c15df71a
Ruben Madila <contact@rubenmadila.com>
2020-01-22 12:11:56 +0000

    search template

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -11,19 +11,18 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<?php indigo_post_thumbnail(); ?>
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			indigo_posted_on();
-			indigo_posted_by();
-			?>
-		</div><!-- .entry-meta -->
+			<div class="entry-meta">
+				<?php
+				indigo_posted_on();
+				indigo_posted_by();
+				?>
+			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
-
-	<?php indigo_post_thumbnail(); ?>
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
