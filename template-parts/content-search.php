352911ffa3e9b3ddc9f4aa4ae1f1c9d413c0adac
Ruben Madila <contact@rubenmadila.com>
2020-01-22 12:11:40 +0000

    style

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,8 +10,8 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php indigo_post_thumbnail(); ?>
 	<header class="entry-header">
+		<?php indigo_post_thumbnail(); ?>
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
