5e5c2b4b2f5e5f69202e6661f71c3cedb0fc703a
Ruben <contact@rubenmadila.com>
2020-02-16 08:25:36 +0000

    controls

--- inc/composer.php
+++ inc/composer.php
@@ -5,3 +5,13 @@
 if ( class_exists( 'WP_Customize_Control' ) ) {
 	require_once  dirname( __FILE__ ) . '/class-customizer-range-value-control/class-customizer-range-value-control.php';
 }
+
+function indigo_customizer_resource_url($url) {
+	return $url.'/inc/customizer-custom-controls/';
+}
+
+add_filter('skyrocket_resource_url', 'indigo_customizer_resource_url');
+
+if ( class_exists( 'WP_Customize_Control' ) ) {
+	require_once  dirname( __FILE__ ) . '/customizer-custom-controls/functions.php';
+}
