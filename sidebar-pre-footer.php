72318f2ed803cfc29f80e51624f281a5378c67a8
Ruben <contact@rubenmadila.com>
2020-03-16 15:42:41 +0000

    align container for pre footer

--- sidebar-pre-footer.php
+++ sidebar-pre-footer.php
@@ -13,7 +13,9 @@ if ( ! is_active_sidebar( 'pre-footer' ) ) {
 ?>
 
 <aside id="tertiary" class="pre-footer">
-	<div class="<?php indigo_conditional_class('indigo_pre_footer_alignment', 'flex', 'widget-area', true); ?>">
-		<?php dynamic_sidebar( 'pre-footer' ); ?>
+	<div class="aligncontainer">
+		<div class="<?php indigo_conditional_class('indigo_pre_footer_alignment', 'flex', 'widget-area', true); ?>">
+			<?php dynamic_sidebar( 'pre-footer' ); ?>
+		</div>
 	</div>
 </aside><!-- #secondary -->
