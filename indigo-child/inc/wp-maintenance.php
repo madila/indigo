2af0a68e4803f726263b4dd55a0ffd9aaec04ce3
Ruben <contact@rubenmadila.com>
2020-04-21 08:33:41 +0100

    wp-maintenance

--- indigo-child/inc/wp-maintenance.php
+++ indigo-child/inc/wp-maintenance.php
@@ -1,48 +1,64 @@
 <?php
-/**
- * Enabling maintenance page.
- *
- * @package WordPress
- * @subpackage Framework
- * @since Framework 0.0.4
- */
 
-/*
- * TODO: Add to documentation the WP_MAINTENANCE constant
- */
+if(!function_exists('ofc_framework_maintenance_redirect')) {
 
-function ofc_framework_maintenance_redirect() {
+	class Indigo_Redirect {
 
-	if(is_front_page()) {
-		return NULL;
+		private $wp_maintenance = false;
+
+		function __constructor() {
+
+			if ( ( defined( 'WP_MAINTENANCE' ) && WP_MAINTENANCE ) ) {
+				$this->wp_maintenance = true;
+				$this->do_maintenance();
+			}
+
+		}
+
+		function do_maintenance() {
+			add_action( 'template_redirect', array($this, 'maintenance_redirect') );
+			add_filter( 'template_include', array($this, 'maintenance_mode'), 99 );
+		}
+
+		function maintenance_redirect() {
+
+			if ( is_front_page() ) {
+				return null;
+			}
+
+			// we do not redirect ajax calls
+			if ( ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+				return null;
+			}
+
+			// Deactivate Maintenance Redirection for Login Routes
+			if ( class_exists( 'Personalize_Login_Plugin' ) && Personalize_Login_Plugin::is_login_url() ) {
+				return null;
+			}
+
+			// we do not redirect visitors or logged-in users that are not using /wp-admin/
+			if ( is_user_logged_in() || is_admin() ) {
+				return null;
+			}
+
+			$front_page   = get_option( 'page_on_front' );
+			$redirect_url = $front_page !== "0" ? get_permalink( $front_page ) : home_url( '/' );
+			wp_redirect( $redirect_url );
+			exit;
+		}
+
+
+		function maintenance_mode( $template ) {
+			if ( ! is_user_logged_in() && is_front_page() ) {
+				return locate_template( 'holding.php' );
+			}
+
+			return $template;
+		}
+
+
 	}
 
-	// we do not redirect ajax calls
-	if ((defined('DOING_AJAX') && DOING_AJAX)) {
-		return NULL;
-	}
+	$wp_maintenance = new Indigo_Redirect();
 
-	// we do not redirect visitors or logged-in users that are not using /wp-admin/
-	if (is_user_logged_in() || is_admin()) {
-		return NULL;
-	}
-
-	$front_page = get_option('page_on_front');
-	$redirect_url = $front_page !== "0" ? get_permalink($front_page) : home_url('/');
-	wp_redirect($redirect_url);
-	exit;
-}
-
-function ofc_framework_maintenance_mode( $template ) {
-
-	if ( !is_user_logged_in() && is_front_page()) {
-		return locate_template( 'holding.php' );
-	}
-
-	return $template;
-}
-
-if((defined('WP_MAINTENANCE') && WP_MAINTENANCE)) {
-	add_action('template_redirect', 'ofc_framework_maintenance_redirect');
-	add_filter('template_include', 'ofc_framework_maintenance_mode', 99);
 }
